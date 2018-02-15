8ab3f12d057980e3dd8637c65b00263ed2bc2995
spsp01 <spsp01@o2.pl>
2018-02-15 22:20:46 +0100

    Revert: index

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from blog.models import Post
 # Create your views here.
+
+def index(request):
+    my_dict= {'Title_head': 'Seo Tools | Strona Główna'}
+    return render(request,'app/app.html', context=my_dict)
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,6 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('app/', include('app.urls'))
 ]
